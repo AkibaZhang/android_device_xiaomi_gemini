59e7f0ed126739d2ab4e3d84f201787dd4c819be
AkibaZhang <zxpgate@gmail.com>
2021-02-24 18:30:44 +0800

    Update lineage_gemini.mk

--- lineage_gemini.mk
+++ lineage_gemini.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2018 The exTHmUI Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,32 +12,46 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 #
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# exTHmUI OFFICIAL
+EXTHM_COMPILERTYPE := OFFICIAL
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit from gemini device
+# Inherit device configuration
 $(call inherit-product, device/xiaomi/gemini/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit exTHmUI product configuration
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
 
-# Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_gemini
-PRODUCT_DEVICE := gemini
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := MI 5
-PRODUCT_MANUFACTURER := Xiaomi
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+# Inherit some common exTHmUI stuff.
+$(call inherit-product, vendor/exthm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="gemini" \
-    PRIVATE_BUILD_DESC="gemini-user 8.0.0 OPR1.170623.032 V9.6.1.0.OAAMIFD release-keys"
+    PRODUCT_NAME="platina" \
+    TARGET_DEVICE="platina" \
+    PRIVATE_BUILD_DESC="gemini-user 10 QKQ1.190910.002 V11.0.1.0.QDTCNXM release-keys"
 
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/gemini/gemini:8.0.0/OPR1.170623.032/V9.6.1.0.OAAMIFD:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys"
 
-TARGET_VENDOR := Xiaomi
+TARGET_VENDOR_PRODUCT_NAME := gemini
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Device identifier
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := gemini
+PRODUCT_NAME := exthm_gemini
+PRODUCT_DEVICE := gemini
+PRODUCT_MODEL := MI 5
