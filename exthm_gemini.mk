885aea2cb4842318250ac41163c910f1d16a149f
AkibaZhang <zxpgate@gmail.com>
2021-02-24 18:36:38 +0800

    Update exthm_gemini.mk

--- exthm_gemini.mk
+++ exthm_gemini.mk
@@ -22,7 +22,7 @@
 #
 
 # exTHmUI OFFICIAL
-EXTHM_COMPILERTYPE := OFFICIAL
+EXTHM_COMPILERTYPE := UNOFFICIAL
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
